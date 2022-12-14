b1e79e0d6eaddfe3ff6d80941a21dbf4c030d1a6
Roman <romichbor@rambler.ru>
2022-12-14 19:17:55 +0300

    new setup

--- README.md
+++ README.md
@@ -4,12 +4,5 @@ You have to install https://github.com/ev-br/mc_lib
 
 To build cy_ising, do
 
-> python setup.py build_ext -i
+> python setup.py build_ext -b ising_builds
 
-To build cy_ising_cluster, do
-
-> python setup_ising_cluster.py build_ext -i
-
-To build cy_ising_cluster, do
-
-> python setup_exact_ising.py build_ext -i
--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 import numpy
 
 import mc_lib
 
-ext = Extension("cy_ising", ["cy_ising.pyx"],
+ising = Extension("cy_ising", ["cy_ising.pyx"],
                 include_dirs = [numpy.get_include(),
                                 mc_lib.get_include()],
                 language='c++',)
 
-setup(ext_modules=[ext],
+
+ising_cluster = Extension("cy_ising_cluster", ["cy_ising_cluster.pyx"],
+                include_dirs = [numpy.get_include(),
+                                mc_lib.get_include()],
+                language='c++',)
+
+
+
+ising_exact = Extension("exact_ising", ["exact_ising.pyx"],
+                include_dirs = [numpy.get_include()],
+                language='c++',)
+
+
+setup(ext_modules=cythonize([ising, ising_cluster, ising_exact], build_dir='ising_builds'),
       cmdclass = {'build_ext': build_ext})
